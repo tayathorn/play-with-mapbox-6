45a7517fc6d7c88d66d4b5050f704659734353ed
tayathorn <tayathorn@thinknet.co.th>
2017-10-24 12:36:43 +0700

    file config

--- PlayMapbox6/src/config.js
+++ PlayMapbox6/src/config.js
@@ -8,12 +8,22 @@ export const Config = {
     one: {
       id: 'get',
       color: '#607D8B',
-      radius: '0.5'
+      radius: '0.2'
     },
     two: {
       id: 'watch',
       color: '#FF9800',
-      radius: '0.6'
+      radius: '0.3'
+    },
+    three: {
+      id: 'getHigh',
+      color: '#4CAF50',
+      radius: '0.4'
+    },
+    four: {
+      id: 'watchHigh',
+      color: '#00BCD4',
+      radius: '0.5'
     }
   }
 }
